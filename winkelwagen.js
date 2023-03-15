688791a5382ef56fbdf3a807e1b9bebbbdb150e3
89031@glr.nl <89031@glr.nl>
2023-03-15 11:54:47 +0100

    commitje

---
+++ winkelwagen.js
@@ -0,0 +1,6 @@
+let prod1 = document.getElementById("plak1").innerHTML;
+let prod2 = document.getElementById("plak2").innerHTML;
+
+function werk(){
+    document.getElementById("producten").innerHTML = prod1 + prod2;
+}
