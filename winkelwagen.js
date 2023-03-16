fb0c12ff5022214b80bbc144ce4c03010394948e
89031@glr.nl <89031@glr.nl>
2023-03-16 09:58:41 +0100

    commit2

--- winkelwagen.js
+++ winkelwagen.js
@@ -1,6 +1,12 @@
 let prod1 = document.getElementById("plak1").innerHTML;
 let prod2 = document.getElementById("plak2").innerHTML;
+let n = 0;
 
 function werk(){
-    document.getElementById("producten").innerHTML = prod1 + prod2;
-}
+        n++;
+        document.getElementById("producten").innerHTML += prod1;
+        document.getElementById("resultaat").innerHTML = "nintendo switch x" + n;
+        
+        
+    }
+
