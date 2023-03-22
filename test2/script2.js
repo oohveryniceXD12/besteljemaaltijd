05509ef32628dfe5c12a7480585245b1e88c40ee
Dander <89800@glr.nl>
2023-03-22 11:58:17 +0100

    jquery

--- test2/script2.js
+++ test2/script2.js
@@ -1,4 +1,20 @@
 
+var velocity = 0.1;
+
+function update(){ 
+var pos = $(window).scrollTop(); 
+$('.infocontainer').each(function() { 
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height()-18;
+    $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px'); 
+   }); 
+   };
+
+ $(window).bind('scroll', update);
+
+
+
 window.onscroll = function() {sliding()};
 
 
