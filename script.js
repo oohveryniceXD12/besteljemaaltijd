8529797b35fa8bb2a176bde044d7f42d5dcfa2a0
Dander <89800@glr.nl>
2023-03-27 09:05:56 +0200

    plus je minnetje

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-if (document.getElementById("plus")){
+document.getElementById("plus").onmouseover = function() {mouseOver()};
+document.getElementById("plus").onmouseout = function() {mouseOut()};
+document.getElementById("plus").onmousedown = function() {toggle()};
+
+document.getElementById("plus").onmouseup = function() {kruis()};
+
+
+var aan =true
+
+function toggle(){
+    aan = !aan;
+    console.log(aan)
+}
+function mouseOver() {
     document.getElementById("plus").classList.add("fa-beat")
 }
+
+function mouseOut() {
+    document.getElementById("plus").classList.remove("fa-beat")
+}
+
+
+function kruis(){
+
+    if(aan == true){
+
+        document.getElementById("plus").classList.remove("fa-solid")
+        document.getElementById("plus").classList.remove("fa-check")
+        document.getElementById("plus").classList.remove("fa-beat")
+        document.getElementById("plus").classList.add("fa-solid")
+        document.getElementById("plus").classList.add("fa-plus")
+    }
+
+    if (aan == false){
+
+
+        document.getElementById("plus").classList.remove("fa-beat")
+        document.getElementById("plus").classList.remove("fa-solid")
+        document.getElementById("plus").classList.remove("fa-plus")
+        document.getElementById("plus").classList.add("fa-solid")
+        document.getElementById("plus").classList.add("fa-check")
+    }
+}
+
+
+
