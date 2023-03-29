16ae9172581c34d0539b608c045cdedd52b2d5d5
Dander <89800@glr.nl>
2023-03-29 11:19:55 +0200

    ja klaar

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let myButtons = document.getElementsByClassName("fa-plus"); 
 Array.from(myButtons).forEach(button => { 
     button.addEventListener("click", (event) => { 
-
+    console.log(button);
+    const id = event.target.getAttribute("id");
     if(event.target.style.color =='rgb(240, 248, 255)')
         event.target.style.color ='rgb(252, 54, 61)'; 
     else{
         event.target.style.color ='rgb(240, 248, 255)';   
     }
+
+    function toevoegen(){
+        console.log(id);
+    }
+    
 }) 
 });
-
 
 window.onscroll = function() {sliding()};
 
@@ -18,9 +23,7 @@ var add = header.offsetTop;
 
 
 function sliding() {
-    console.log(window.pageYOffset);
     if (window.pageYOffset > add) {
-        console.log("hit");
         document.getElementById("pro1").style.top = '50%';
         document.getElementById("pro2").style.top = '50%';
         document.getElementById("pro3").style.top = '50%';
